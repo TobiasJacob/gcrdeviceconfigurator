b8f51c23405ceee3f6eb7d2895f8bcbf7524f12e
TobiasJacob <tobi.jacob@gmx.net>
2022-12-23 08:33:52 +0100

    Added export profile function

--- lib/ui/profile_tile.dart
+++ lib/ui/profile_tile.dart
@@ -1,6 +1,10 @@
+import 'dart:convert';
+
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:gcrdeviceconfigurator/pages/profile_page.dart';
 import 'package:provider/provider.dart';
+import 'dart:io';
 
 import '../data/database.dart';
 import '../data/profile.dart';
@@ -33,16 +37,26 @@ class ProfileTile extends StatelessWidget {
           Expanded(
               child: Text(profile.name,
                   style: const TextStyle(color: Colors.black, fontSize: 18))),
-          MaterialButton(
-            onPressed: () {
-              database.deleteProfileIfMoreThanOne(profileKey);
+          PopupMenuButton<String>(
+            onSelected: (ev) {
+              switch (ev) {
+                case "Export":
+                  exportProfile(context);
+                  break;
+                case "Delete":
+                  showDeleteConfirmation(context);
+                  break;
+                default:
+              }
             },
-            shape: const CircleBorder(),
-            materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
-            minWidth: 0,
-            child: const Icon(
-              Icons.delete,
-            ),
+            itemBuilder: (BuildContext context) {
+              return {'Export', 'Delete'}.map((String choice) {
+                return PopupMenuItem<String>(
+                  value: choice,
+                  child: Text(choice),
+                );
+              }).toList();
+            },
           ),
           MaterialButton(
             onPressed: () {
@@ -62,5 +76,101 @@ class ProfileTile extends StatelessWidget {
         ],
       ),
     );
+  }
+
+  Future<void> exportProfile(BuildContext context) async {
+    String? outputFile = await FilePicker.platform.saveFile(
+        dialogTitle: 'Save Your File to desired location',
+        fileName: "${profile.name}.json",
+        allowedExtensions: ["json"],
+        type: FileType.custom);
+    if (outputFile == null) return;
+
+    try {
+      if (!outputFile.endsWith(".json")) {
+        outputFile += ".json";
+      }
+      File returnedFile = File(outputFile);
+      if (await returnedFile.exists()) {
+        final text = "File $outputFile exists. Overwrite?";
+
+        AlertDialog alert = AlertDialog(
+          title: const Text("Overwrite"),
+          content: Text(text),
+          actions: [
+            TextButton(
+              child: const Text("No"),
+              onPressed: () {
+                Navigator.pop(context, false);
+              },
+            ),
+            TextButton(
+              child: const Text("Yes"),
+              onPressed: () {
+                Navigator.pop(context, true);
+              },
+            ),
+          ],
+        );
+
+        // show the dialog
+        final confirmation = await showDialog<bool>(
+          context: context,
+          builder: (BuildContext context) {
+            return alert;
+          },
+        );
+
+        if (confirmation != true) {
+          return;
+        }
+      }
+      await returnedFile.writeAsBytes(jsonEncode(profile.toJSON()).codeUnits);
+    } catch (e) {
+      AlertDialog alert = AlertDialog(
+        title: const Text("Error"),
+        content: Text(e.toString()),
+        actions: [
+          TextButton(
+            child: const Text("OK"),
+            onPressed: () {},
+          ),
+        ],
+      );
+
+      // show the dialog
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return alert;
+        },
+      );
+    }
+  }
+
+  Future<bool> showDeleteConfirmation(BuildContext context) async {
+    final database = Provider.of<Database>(context, listen: false);
+    return await showDialog<bool>(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: const Text('Exit App'),
+            content: const Text('Do you want to delete the profile?'),
+            actions: [
+              ElevatedButton(
+                onPressed: () {},
+                //return false when click on "NO"
+                child: const Text('No'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  database.deleteProfileIfMoreThanOne(profileKey);
+                },
+                //return true when click on "Yes"
+                child: const Text('Yes'),
+              ),
+            ],
+          ),
+        ) ??
+        false;
   }
 }
