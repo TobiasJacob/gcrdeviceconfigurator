55a0400d25ff8ed635e2aec3ee744c9343dd6fe9
Tobias Jacob <tobi.jacob@gmx.net>
2023-02-11 12:10:51 +0100

    Removed dead imports

--- lib/pages/home/profile_list.dart
+++ lib/pages/home/profile_list.dart
@@ -1,11 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:gcrdeviceconfigurator/data/profile_view_provider.dart';
 import 'package:gcrdeviceconfigurator/data/settings_provider.dart';
 import 'package:gcrdeviceconfigurator/i18n/languages.dart';
 import 'package:gcrdeviceconfigurator/pages/home/profile_tile.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-
-import '../profile_page.dart';
 
 class ProfileList extends ConsumerWidget {
   const ProfileList({super.key});
